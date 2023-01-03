32ee6ea3d7ef4f0c741eabb29ad02207472a8868
gobinddas <48596268+gobinddas@users.noreply.github.com>
2023-01-03 15:18:50 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $('.bar').click(function() {
+        $(".resnav").toggle();
+    })
+
+    $('.css-1jdsho5').click(function() {
+        $('.australia').css("background-color", "blue");
+        $('.nepal').css("background-color", "transparent");
+        $('.usa').css("background-color", "transparent");
+
+
+
+    })
+
+    $('.css-1fjpsx8').click(function() {
+        $('.nepal').css("background-color", "blue");
+        $('.australia').css("background-color", "transparent");
+        $('.usa').css("background-color", "transparent");
+
+
+    })
+    $('.css-1mucvq7').click(function() {
+        $('.usa').css("background-color", "blue");
+        $('.nepal').css("background-color", "transparent");
+        $('.australia').css("background-color", "transparent");
+
+    })
+
+
+
+
+
+
+
+
+})
